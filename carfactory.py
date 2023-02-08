4de8ebb7ee1349290db4067966230830452721ba
kartik kain <kartik.kain2000@gmail.com>
2023-02-08 17:19:15 +0530

    Refactored

--- carfactory.py
+++ carfactory.py
@@ -3,3 +3,14 @@ from car import Car
 class CarFactory(Car,ABC):
     def __init__(self,engine,battery):
         super().__init__(engine,battery)
+
+    def create_calliope(self):
+        return super().need_service()
+    def create_glissade(self):
+        return super().need_service()
+    def create_palindrome(self):
+        return super().need_service()
+    def create_rorschach(self):
+        return super().need_service()
+    def create_thovex(self):
+        return super().need_service()
--- engine/model/calliope.py
+++ engine/model/calliope.py
@@ -1,7 +1,5 @@
 from datetime import datetime
-
 from engine.capulet_engine import CapuletEngine
-
 
 class Calliope(CapuletEngine):
     def needs_service(self):
